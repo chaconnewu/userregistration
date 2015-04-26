1f30373de4187daead8f9037bb142e6b60e91aa5
Yu Wu <chaconnewu@gmail.com>
2015-04-26 09:33:53 -0400

    added main interface

---
+++ app/routes.js
@@ -0,0 +1,37 @@
+module.exports = function(app, passport) {
+    app.get('/', function(req, res) {
+        res.render('index.ejs');
+    });
+
+    app.get('/login', function(req, res) {
+        res.render('login.ejs', {
+            message: req.flash('loginMessage')
+        });
+    });
+
+    // app.post('/login', do all our passport stuff here);
+
+    app.get('/signup', function(req, res) {
+        res.render('signup.ejs', {
+            message: req.flash('signupMessage')
+        });
+    });
+
+    // app.post('/signup', do all our passport stuff here);
+
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {
+            user: req.user
+        });
+    });
+};
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    res.redirect('/');
+}
+
+
---
+++ server.js
@@ -0,0 +1,36 @@
+// server.js
+// set up ========================
+
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 8080,
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    flash = require('connect-flash'),
+    morgan = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
+
+// set up express
+app.use(morgan('dev'));     // log every request to the console
+app.use(cookieParser());    // read cookies (for authentication)
+app.use(bodyParser());      // get information from html forms
+
+app.set('view engine', 'ejs');   // set up ejs for templating
+
+// passport set up
+app.use(session({secret: 'thisiswuyu'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./app/routes.js')(app, passport);
+
+app.listen(port);
+console.log('The magic happens on port ' + port);
+
+
